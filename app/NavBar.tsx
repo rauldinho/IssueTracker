eb3ba5b43944f7561c96e01a381df070dada0d3d
Raul Requena <requenaraul@gmail.com>
2024-06-21 17:51:00 -0400

    Display active navbar link conditionally

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
 
 const NavBar = () => {
+    const currentPath = usePathname();
+
     const links = [
         { label: "Dashboard", href: "/" },
         { label: "Issues", href: "/issues" },
@@ -16,7 +22,11 @@ const NavBar = () => {
             <ul className="flex space-x-6">
                 {links.map((link) => (
                     <Link
-                        className="text-zinc-500 hover:text-zinc-800 transition-colors"
+                        className={`${
+                            link.href === currentPath
+                                ? "text-zinc-9"
+                                : "text-zinc-500"
+                        }  hover:text-zinc-800 transition-colors`}
                         key={link.href}
                         href={link.href}
                     >
